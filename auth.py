f774ccf31fc1a18b0aa1475d7fc569fb1a0dba96
mustaf1nx <mandarinchick001@gmail.com>
2026-04-18 01:21:10 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "zein-academy-secret-key-change-in-producti
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 480))
 
-pwd_context = CryptContext(schemes=["bcrypt", "sha256_crypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", bcrypt__rounds=12)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
