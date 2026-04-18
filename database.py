e65aaf1eb8c145b5e611db93b18cd49395cb191c
mustaf1nx <mandarinchick001@gmail.com>
2026-04-19 03:30:27 +0500

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./zein.db")
+DATABASE_URL = os.getenv("postgresql://postgres:qjkmXgbHofxdFvtbNAWsqDamXCdvdFyB@roundhouse.proxy.rlwy.net:47051/railway", "sqlite:///./zein.db")
 
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
